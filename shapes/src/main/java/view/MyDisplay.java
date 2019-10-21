bfeac12b3435790f63e84156a82cb6c97a4f5076
bahth18 <thierno.bah@dauphine.eu>
2019-10-21 13:14:36 +0200

    réponses à quelques questions de l'exercice Graphiques D2.

--- shapes/src/main/java/view/MyDisplay.java
+++ shapes/src/main/java/view/MyDisplay.java
@@ -1,11 +1,22 @@
 package view;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class MyDisplay extends JPanel{
+	
+	//Exercice 1. Graphiques D2
+	//Question 1.4
+	
+	public void paintComponent(Graphics g) { // Redéfinition de la méthode paintComponent
+		
+		super.paintComponent(g);
+		g.drawLine(15, 10, 100, 50);
+		 
+	}
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Java Avancé - Graphic Display");
