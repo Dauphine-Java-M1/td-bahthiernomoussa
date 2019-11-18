b87ff88ff53aa7d81649d48b2e6fe48d1755cce9
bahth18 <thierno.bah@dauphine.eu>
2019-11-18 14:00:30 +0100

    creation du fichier Panel.java

---
+++ iterables/src/main/java/fr/dauphine/ja/bahthiernomoussa/iterables/Panel.java
@@ -0,0 +1,24 @@
+package fr.dauphine.ja.bahthiernomoussa.iterables;
+
+import java.util.Iterator;
+
+/**
+ * Hello world!
+ *
+ */
+public class Panel<E> implements Iterable<E>
+{
+	
+	//Question 1
+	
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour le monde!!" );
+    }
+}
