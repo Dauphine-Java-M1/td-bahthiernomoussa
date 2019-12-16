cf4cd2baeda304e521d296a50ebb5110f845f1f4
bahth18 <thierno.bah@dauphine.eu>
2019-12-16 15:19:21 +0100

    Création de la classe SynchronizedListWithMultipleReaders.java

---
+++ readerwriters/src/main/java/fr/dauphine/ja/bahthiernomoussa/readerwriters/SynchronizedListWithMultipleReaders.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.bahthiernomoussa.readerwriters;
+
+/**
+ * Hello world!
+ *
+ */
+public class SynchronizedListWithMultipleReaders
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
